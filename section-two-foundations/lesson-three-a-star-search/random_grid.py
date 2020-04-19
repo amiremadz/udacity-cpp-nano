7ce649a4c62028f8fe20b36e716363c3036e2e15
Michael Harris <michael.harris@block.one>
2020-04-18 23:21:05 -0400

    Fix dimensions bug. Use rows x cols instead of width x height to conform to row-major vector search

--- section-two-foundations/lesson-three-a-star-search/random_grid.py
+++ section-two-foundations/lesson-three-a-star-search/random_grid.py
@@ -12,21 +12,24 @@ import time
 
 """
 Creating command line arguments for the user to pass.
-User shall pass the width and length of the 2D vector space.
+User shall pass the rows and columns dimensions of the 2D vector space.
 """
-parser = argparse.ArgumentParser(description='Give the width and height of the grid.csv\n')
-parser.add_argument('width', type=int, help='Width of the grid.csv')
-parser.add_argument('height', type=int, help='Height of the grid.csv')
+parser = argparse.ArgumentParser(description='Give the size of rows and columns for the grid.csv\n')
+parser.add_argument('rows', type=int, help='Rows in the grid.csv')
+parser.add_argument('columns', type=int, help='Columns in the grid.csv')
 args = parser.parse_args()
 
 # Constants passed from user
-W = args.width
-H = args.height
+Rows = args.rows
+Cols = args.columns
 
 
-def create_random_grid(w: int, h: int):
+def create_random_grid(rows: int, cols: int):
     """Creates a csv file comprising of a randomly generated
     2D vector comprising of only 0 and 1.
+
+    Dimensions correspond the the rows and cols variables
+    passed into the function.
 
     Probabilities a cell having either 0 or 1:
 
@@ -35,7 +38,7 @@ def create_random_grid(w: int, h: int):
 
     This is so we can create a random csv file for our A* algorithm
     to read from that has a large chance of having a solvable grid space."""
-    df = pd.DataFrame(np.random.random_integers(0, 10, (w, h)))
+    df = pd.DataFrame(np.random.random_integers(0, 10, (rows, cols)))
     df = df.applymap(lambda x: 0 if x >= 2 else 1)
     df.iloc[0, 0], df.iloc[-1, -1] = 0, 0  # this stops the upper left and lower right corners from being 1
     if not os.path.exists('grids'):
@@ -52,6 +55,6 @@ if __name__ == '__main__':
     print('+---------------+')
     print('|   |   |   |   |')
     print('+---------------+')
-    create_random_grid(W, H)
+    create_random_grid(Rows, Cols)
     time.sleep(2)
-    print(f'\n\nGrid with dimensions of {W}x{H} has been created in the /grid directory as grid.csv\n\n')
+    print(f'\n\nGrid with dimensions of {Rows}x{Cols} has been created in the /grid directory as grid.csv\n\n')
